deb062506b60b052de7551f7d38d1ffe74267582
fabiancrc124 <fabiancrc124@gmail.com>
2024-11-05 07:39:20 -0500

    Se hacen distintos componentes, que nos permite dinamicamente estudiar como funcion los modelos y de como interactuan entre si

---
+++ src/app/dbz/pages/main-page.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+import { Character } from '../interfaces/character.interface';
+
+@Component({
+  selector: 'app-dbz-main-page',
+  templateUrl: './main-page.component.html'
+})
+
+export class MainPageComponent {
+
+  public characters: Character[] = [{
+    name: 'Krilin',
+    power: 1000
+  },{
+    name:'Goku',
+    power:9500
+  },
+  {
+    name: 'Picoro',
+    power: 1200
+  }
+
+];
+
+
+
+
+}
